0908ca1f5b247269bd12c5efbe2f5fc2f66f815b
aryawpratama <aryawpratama@gmail.com>
2022-07-20 13:11:08 +0700

    exit message

--- question1-3/src/index.ts
+++ question1-3/src/index.ts
@@ -1,12 +1,18 @@
+import colors from "colors";
 import question2 from "./question/question2";
 import question1 from "./question/question1";
 import question3 from "./question/question3";
-
 (async () => {
+  colors.enable();
   console.clear();
   await question1();
   console.clear();
   await question2();
   console.clear();
   await question3();
+  console.log(
+    `\n
+  All done!! Thank you for reviewing my test. :)
+  `.green
+  );
 })();
